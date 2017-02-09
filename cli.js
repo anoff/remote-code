7be4ec127374c277f2a698217aa0a9c90c264822
anoff <offenhaeuser@gmail.com>
2017-02-09 23:45:22 +0100

    :scissors: refactor: fit index/cli pattern
    undefined

--- cli.js
+++ cli.js
@@ -1,20 +1,27 @@
 #!/usr/bin/env node
-'use strict';
+const split = require('split');
 const meow = require('meow');
-const remoteCode = require('.');
+const RemoteCode = require('.');
 
-const cli = meow(`
-	Usage
-	  $ remote-code [input]
+const sshSettings = {
+	host: '192.168.2.5',
+	port: 2221,
+	username: 'pi',
+	privateKey: require('fs').readFileSync('/Users/anoff/.ssh/id_pi'),
+	keepaliveInterval: 500,
+	readyTimeout: 2000
+};
 
-	Options
-	  --foo  Lorem ipsum [Default: false]
+const options = {
+	ssh: sshSettings,
+	project: '/developer/anoff/'
+};
 
-	Examples
-	  $ remote-code
-	  unicorns & rainbows
-	  $ remote-code ponies
-	  ponies & rainbows
-`);
+const remoteCode = new RemoteCode(options);
+const liveSsh = remoteCode.ssh.liveReload;
+liveSsh.connect(sshSettings);
 
-console.log(remoteCode(cli.input[0] || 'unicorns'));
+// make node process interactive by passing stdin to the remote shell
+process.stdin.pipe(split()).on('data', line => {
+	liveSsh.send(line);
+});
--- index.js
+++ index.js
@@ -1,33 +1,15 @@
-const split = require('split');
-const Ssh = require('./lib/ssh');
-const path = require('path');
 const EventEmitter = require('events');
-
-const sshSettings = {
-	host: '192.168.2.5',
-	port: 2221,
-	username: 'pi',
-	privateKey: require('fs').readFileSync('/Users/anoff/.ssh/id_pi'),
-	keepaliveInterval: 500,
-	readyTimeout: 2000
-};
+const Ssh = require('./lib/ssh');
 
 class RemoteCode {
-	constructor() {
+	constructor(opts) {
+		this.options = opts;
 		this.emitter = new EventEmitter();
 		this.ssh = {
 			liveReload: new Ssh()
 		};
+		return this;
 	}
 }
-const settings = {
-	project: '/developer/anoff/'
-};
-const ssh = new Ssh();
-ssh.connect(sshSettings);
 
-// make node process interactive by passing stdin to the remote shell
-process.stdin.pipe(split()).on('data', line => {
-	ssh.send(line);
-});
-
+module.exports = RemoteCode;
