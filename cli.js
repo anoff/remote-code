e96f95b11f1eefbffaa794415bb20f6b0a0d7bc8
anoff <offenhaeuser@gmail.com>
2017-02-11 00:31:48 +0100

    :scissors: refactor: move cli-ish code
    undefined

--- cli.js
+++ cli.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 const split = require('split');
 const meow = require('meow');
+const chalk = require('chalk');
 const RemoteCode = require('.');
 
 const sshSettings = {
@@ -21,10 +22,36 @@ const options = {
 
 const remoteCode = new RemoteCode(options);
 const liveSsh = remoteCode.ssh.liveReload;
+let chunks = '';
+let ignoreStdOut = []; // array to hold commands that should be kept from showing up on stdout
+liveSsh.getEventEmitter()
+	.on('connect', s => console.log(chalk.magenta(s)))
+	.on('close', s => {
+		console.log(chalk.magenta(s));
+		// end the node process once livechannel is gone (e.g. typing exit)
+		process.exit(0);
+	})
+	.on('error', s => console.log(chalk.bold.red(s)))
+	.on('data', data => {
+		if (ignoreStdOut.length > 0) {
+			chunks += data;
+			ignoreStdOut.forEach((cmd, i) => {
+				if (cmd + '\r\n' === chunks || cmd + '\n' === chunks) {
+					chunks = '';
+					ignoreStdOut.splice(i, 1);
+				} else if (chunks.slice(-1) === '\n') {
+					ignoreStdOut.forEach(() => ignoreStdOut.pop());
+				}
+			});
+		} else {
+			process.stdout.write(chalk.blue(data.toString()));
+		}
+	});
 liveSsh.connect(sshSettings);
 //remoteCode.syncCode();
 
 // make node process interactive by passing stdin to the remote shell
 process.stdin.pipe(split()).on('data', line => {
+	ignoreStdOut.push(line); // prevent this line from showing up in stdout
 	liveSsh.send(line);
 });
