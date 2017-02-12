14f244a3ba4a15e3ef7c79598297956589e8e256
anoff <offenhaeuser@gmail.com>
2017-02-12 23:09:01 +0100

    :star2: feat: add cli options
    undefined

--- cli.js
+++ cli.js
@@ -1,19 +1,56 @@
 #!/usr/bin/env node
+const path = require('path');
+const url = require('ssh-url');
 const chalk = require('chalk');
+const meow = require('meow');
 const RemoteCode = require('.');
 
+
+const cli = meow(`
+	Usage
+		$ remote-code <[user@]host>
+
+	Options
+		--port, -p Custom port [22]
+		--identity-file, -i SSH keyfile
+		--user, -u SSH username
+		--password, -P SSH password (not supported)
+		--source, -s directory to synchronize (local) [CWD]
+		--target, -t remote location to sync to [~]
+
+	Examples
+		$ remote-code user@192.168.0.4
+		$ remote-code -p 23 -i ~/.ssh/id_rsa --user admin 192.168.0.4
+`, {
+	alias: {
+		p: 'port',
+		i: 'identity-file',
+		u: 'user',
+		P: 'password',
+		s: 'source',
+		t: 'target'
+	}
+});
+
+// parse argument for user/host
+const parts = url.parse(cli.input[0]);
 const options = {
 	ssh: {
-		host: '192.168.2.5',
-		port: 2221,
-		username: 'pi',
-		keyfilePath: '/Users/anoff/.ssh/id_pi',
+		host: parts.hostname,
+		port: cli.flags.port || 22,
+		username: cli.flags.user || parts.user,
+		keyfilePath: cli.flags.identityFile,
+		password: cli.flags.password,
 		keepaliveInterval: 500,
 		readyTimeout: 2000
 	},
-	source: '/developer/anoff/dummyApp',
-	target: '/home/pi/dummyApp'
+	source: cli.flags.source,
+	target: cli.flags.target
 };
+
+// check for missing options
+!options.ssh.host && console.log('Please provide a valid host') && process.exit();
+!options.ssh.username && console.log('Please provide a valid username') && process.exit();
 
 const remoteCode = new RemoteCode(options);
 
